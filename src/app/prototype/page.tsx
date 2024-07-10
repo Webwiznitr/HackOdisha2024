3e1ff9f80dd6c0c6bdcf7ab085394844cddfb8da
Abhishek Sahu <kalisahu752@gmail.com>
2024-07-10 23:33:06 +0530

    add image folder

--- src/app/prototype/page.tsx
+++ src/app/prototype/page.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex justify-center">
       Testing Home Page
       {/* Import and add your respective component here */}
 
 
-      
+
     </main>
   );
 }
