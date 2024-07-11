ef4fd8622f6b12c21cb0ea95d47b57246e7e85bc
ayussh-2 <ayushatschool5@gmail.com>
2024-07-12 01:17:57 +0530

    feat: add sponsors component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,33 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Dela_Gothic_One, Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+    subsets: ["latin"],
+    variable: "--font-inter",
+    weight: ["100", "300", "400", "700"],
+});
+const delaGothicOne = Dela_Gothic_One({
+    subsets: ["latin"],
+    variable: "--font-dela-gothic-one",
+    weight: ["400"],
+});
 
 export const metadata: Metadata = {
-  title: "HackOdisha 4.0",
-  description: "hackodisha",
+    title: "HackOdisha 4.0",
+    description: "hackodisha",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${inter.variable} ${delaGothicOne.variable}`}>
+                {children}
+            </body>
+        </html>
+    );
 }
