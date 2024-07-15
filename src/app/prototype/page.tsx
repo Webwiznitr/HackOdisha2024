5d17c9c2294502fc2374f4de2ca9e88a8a3c6a46
Payaldas <payaldaskunu30@gmail.com>
2024-07-15 20:28:23 +0530

    feat:aboutus

--- src/app/prototype/page.tsx
+++ src/app/prototype/page.tsx
@@ -1,11 +1,15 @@
+import AboutUs from "@/components/AboutUs/About";
 import Sponsors from "@/components/Sponsors/Sponsors";
 import Image from "next/image";
+import Timeline from "./Timeline/Timeline";
 
 export default function Home() {
     return (
         <main className="flex justify-center flex-col">
             Testing Home Page
             {/* Import and add your respective component here */}
+            <AboutUs/>
+            <Timeline/>
             <Sponsors />
         </main>
     );
