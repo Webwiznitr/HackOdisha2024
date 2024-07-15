601b86d6e3346251db74b53a5597c7c472636794
Subhransu Dalei <subhransu2004@gmail.com>
2024-07-15 13:07:43 +0530

    minor changes(2)

--- src/components/AreUReady/AreUReady.tsx
+++ src/components/AreUReady/AreUReady.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AreUReady = () => {
   return (
     <section className='bg-[#634752] relative'>
-      <div className='flex flex-col items-center justify-center py-[6rem] md:py-[6rem] lg:py-[8rem]'>
+      <div className='flex flex-col items-center justify-center pt-[6rem] pb-[7rem] md:py-[6rem] lg:py-[8rem]'>
       <video
       src='https://res.cloudinary.com/dttynw5li/video/upload/v1720981434/About_us_animation-2_vml0cl.mp4' 
       width={60} 
@@ -13,7 +13,7 @@ const AreUReady = () => {
       playsInline
       preload='none' 
       disablePictureInPicture
-      className='absolute top-6 right-4 sm:block sm:w-[5rem] md:w-[7rem] lg:mr-20'
+      className='absolute top-6 right-4 sm:block sm:w-[5rem] md:w-[7rem] lg:mr-20 md:pt-4'
       >
       </video>
       <h2 className='text-white text-lg sm:text-2xl lg:text-4xl font-archivoBlack'>ARE YOU READY?</h2>
@@ -42,7 +42,7 @@ const AreUReady = () => {
       playsInline
       preload='none'
       disablePictureInPicture
-      className='absolute sm:left-5 sm:bottom-6 bottom-2 left-4 sm:block sm:w-[6rem] md:w-[8.5rem] lg:pb-10 lg:ml-20'
+      className='absolute sm:left-5 bottom-6 left-4 sm:block sm:w-[6rem] md:w-[8.5rem] lg:pb-10 lg:ml-20'
       >
       </video>
       </div>
