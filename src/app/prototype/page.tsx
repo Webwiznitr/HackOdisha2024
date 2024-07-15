bfbaa67c9a42a851d6dcbfa8c91221d0ce0cd4b5
Siddharth <siddharthnmishra7@gmail.com>
2024-07-15 17:13:51 +0530

    fix: corrected file structure

--- src/app/prototype/page.tsx
+++ src/app/prototype/page.tsx
@@ -1,12 +1,12 @@
 import Sponsors from "@/components/Sponsors/Sponsors";
-import Timeline from "./Timeline/Timeline";
+import Timeline from "@/components/Timeline/Timeline"
 
 export default function Home() {
     return (
       <main className="flex justify-center flex-col">
         Testing Home Page
         {/* Import and add your respective component here */}
-        <Timeline />
+        <Timeline/>
         <Sponsors />
       </main>
     )
