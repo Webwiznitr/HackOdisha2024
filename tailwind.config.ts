0d3ebd8dbd8b93bc1832ea9d61969eb383723c3d
ayussh-2 <ayushatschool5@gmail.com>
2024-07-17 17:39:33 +0530

    refactor: remover team component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,45 +1,45 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
 
-      fontFamily: {
-        inter: "var(--font-inter)",
-        "dela-gothic": "var(--font-dela-gothic-one)",
-        archivoBlack: "var(--archivo-black)",
-        archivo: "var(--archivo)",
-        oxanium:"var(--font-oxanium)",
-      },
-      
-      colors:{
-        customPink: '#624353',
-      },
+            fontFamily: {
+                inter: "var(--font-inter)",
+                "dela-gothic": "var(--font-dela-gothic-one)",
+                archivoBlack: "var(--archivo-black)",
+                archivo: "var(--archivo)",
+                oxanium: "var(--font-oxanium)",
+            },
 
-      screens: {
-        'custom-1080': {'min': '1080px'},
-        'custom-940': {'max': '940px'},
-        'custom-770': {'max': '770px'},
-        'custom-535': {'max': '535px'},
-        'custom-500': {'max': '500px'},
-        'custom-420': {'max': '420px'},
-        'custom-385': {'max': '385px'},
-        'custom-355': {'max': '355px'},
-        'custom-345': {'max': '330px'},
+            colors: {
+                customPink: "#624353",
+            },
 
-      },
+            screens: {
+                xsm: "300px",
+                "custom-1080": "1080px",
+                "custom-940": "940px",
+                "custom-770": "770px",
+                "custom-535": "535px",
+                "custom-500": "500px",
+                "custom-420": "420px",
+                "custom-385": "385px",
+                "custom-355": "355px",
+                "custom-345": "345px",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
