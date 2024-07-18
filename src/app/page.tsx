480d5d8439e64411f399319812a7f34a300e675d
Ashwani Senapati <senapatiashwani47@gmail.com>
2024-07-18 20:30:21 +0530

    chore:minor fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import Faq from "@/components/FAQ/Faq.js";
 export default function Home() {
   return (
     <main className="">
-  
+    <Faq/>
     </main>
   );
 }
