62250d9209496d8f084f396dc135a5e11d4fae13
Sanket Y <124805576+sanketyelave@users.noreply.github.com>
2024-08-03 19:07:35 +0530

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -174,7 +174,9 @@ const Navbar: React.FC = () => {
     e.currentTarget.style.boxShadow = "4px 4px 0px 0px rgba(0, 0, 0, 0.75)";
   }}
 >
+  <a href="https://discord.com/invite/FqQjHUgmwS">
   Join Discord
+  </a>
 </button>
 
                   </div>
